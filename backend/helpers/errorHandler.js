bda59c88812dbd92ef0e73c57595dd98caf7a84b
Andrew Yupin <brolo1341@gmail.com>
2022-10-02 11:08:49 +0300

    added errorsHandling

---
+++ backend/helpers/errorHandler.js
@@ -0,0 +1,12 @@
+function errorHandler(err,req, res, next) {
+    if(err.name == 'UnauthorizedError'){
+      return res.status(401).json({
+          message: 'The user is not authorized',
+          description: 'No authorization token was found'
+        })
+      }
+
+    return res.status(500).json({message: err})
+}
+
+module.exports = errorHandler;
