7c263a2d40b16528c3b9cf755c23c97f0687565d
Andrew Yupin <brolo1341@gmail.com>
2022-10-02 11:27:17 +0300

    set non-required token for a few path

--- backend/helpers/errorHandler.js
+++ backend/helpers/errorHandler.js
@@ -1,3 +1,4 @@
+
 function errorHandler(err,req, res, next) {
     if(err.name == 'UnauthorizedError'){
       return res.status(401).json({
--- backend/helpers/jwt.js
+++ backend/helpers/jwt.js
@@ -1,10 +1,19 @@
 var { expressjwt: jwt } = require("express-jwt");
 const secret = process.env.secret_key;
+const API = process.env.API_URL;
 
 function authJwt() {
   return jwt({
     secret,
     algorithms: ["HS256"],
+  }).unless({
+    path: [
+      {url: /\/api\/v1\/products(.*)/ , methods : ['GET', 'OPTIONS']},
+      {url: /\/api\/v1\/categories(.*)/ , methods : ['GET', 'OPTIONS']},
+      `${API}/users/login`,
+      `${API}/users/register`
+
+    ]
   });
 }
 
